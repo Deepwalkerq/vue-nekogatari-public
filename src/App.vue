071caa34bbc472a783c3e508d1c1a98d4f9d24aa
Deepwalkerq <nelsen278@gmail.com>
2021-08-02 19:49:42 +0300

    NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">home</router-link> |
-    <router-link to="/members">members</router-link>
-  </div>
+  <NavBar />
   <router-view/>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar'
+
+export default {
+  name: 'app',
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style>
 #app {
@@ -13,18 +21,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="nav">
+        <router-link to="/">home</router-link> |
+        <router-link to="/members">members</router-link>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+#nav {
+  padding: 30px;
+  
+  a {
+  font-weight: bold;
+  color: #2c3e50;
+  }
+
+  a.router-link-exact-active {
+  color: #42b983;
+  }
+
+}
+</style>
